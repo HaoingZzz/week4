28090832f44f5702dc07514b31f9c690491a3b9d
HaoingZzz <zceihz0@ucl.ac.uk>
2022-11-02 01:00:08 +0800

    p6

--- starter_code/problem11.py
+++ starter_code/problem11.py
@@ -1,3 +1,4 @@
+from operator import index
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -12,10 +13,19 @@ if __name__ == '__main__':
 
     # 1. Create two new dataframes, one with winter data and one with summer data using .loc
     # Syntax will be `df.loc[df['Column'] condition]`. You can use conditions such as == (equals)
-
+    df_summer = df.loc[df['Type'] == 'Summer']  
+    # print(df_summer)
+    df_winter = df.loc[df['Type'] == 'Winter']
+    print(df_winter)
     # 2. Reset the index of each of the new dataframes e.g. df_summer.reset_index(drop=True, inplace=True)
-
+    df_summer.reset_index(drop = True, inplace = True)
+    df_winter.reset_index(drop = True, inplace = True)
     # 3. Create two line plots, for each x will be 'Year' and y will be the variable 'cols' (see line 8)
-
+    df_summer.plot.line()
+    plt.xticks(rotation=90)
+    xticks = df_summer.index
+    df.set_xticks(xticks)
+    df.set_xticklabels(df['Year'])
+    
     # 4. Show the plots
-
+    plt.show()
