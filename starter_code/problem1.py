75364865ce44eac909922d0e647c70497c4566d4
HaoingZzz <zceihz0@ucl.ac.uk>
2022-11-01 15:18:23 +0800

    que_1

--- starter_code/problem1.py
+++ starter_code/problem1.py
@@ -1,3 +1,14 @@
+import pandas as pd
+from pathlib import Path
+
+
+def prepare_data():
+
+    paralympics_raw_csv = Path(__file__).parent.parent.joinpath('data', 'paralympics_raw.csv')
+    df = pd.read_csv(paralympics_raw_csv)
+    pd.set_option('display.max_rows', df.shape[0] + 1)
+    pd.set_option('display.max_columns', df.shape[1] + 1)
+    return df
 
 
 if __name__ == '__main__':
@@ -9,5 +20,7 @@ if __name__ == '__main__':
     '''
 
     # Add code to change the pandas display options for the max_rows and max_columns (optional)
-
+    
     # Add a line of code to print the dataframe contents
+    df_raw = prepare_data()
+    print(df_raw)
